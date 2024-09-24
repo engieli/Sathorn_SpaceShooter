d0e43d8dd35d5483f38006b8791b6cc79227940f
DESKTOP-KBV0CC1\Eng <sathorn@sheridancollege.ca>
2024-09-24 02:33:42 -0400

    Finished Task 1 and 2

--- Assets/Scripts/Controllers/Enemy.cs
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -3,8 +3,41 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
-    private void Update()
+    public GameObject enemyPrefab;  
+    public float spawnInterval = 5f;  
+    public float initialSpeed = 2f;  
+    public float acceleration = 0.5f; 
+    public Vector3 movementDirection = Vector3.forward;
+
+    private float currentSpeed;
+    private float timeSinceLastSpawn;
+
+    void Start()
     {
+        currentSpeed = initialSpeed;
+        timeSinceLastSpawn = 0f;
     }
 
+    void Update()
+    {
+     
+        timeSinceLastSpawn += Time.deltaTime;
+
+        
+        if (timeSinceLastSpawn >= spawnInterval)
+        {
+            SpawnEnemy();
+            timeSinceLastSpawn = 0f; 
+        }
+
+        
+        currentSpeed += acceleration * Time.deltaTime;
+        transform.Translate(movementDirection * currentSpeed * Time.deltaTime);
+    }
+
+    void SpawnEnemy()
+    {
+       
+        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+    }
 }
--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -8,53 +8,112 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public Transform bombsTransform;
+    public float maxSpeed = 5f;
+    public float accelerationTime = 2f;
+    private float acceleration;
+    private Vector3 currentVelocity = Vector3.zero;
 
-   // public Vector3 velocity = new Vector3(1f, 0f);
 
-    
+    // public Vector3 velocity = new Vector3(1f, 0f);
+    void Start()
+    {
+
+        acceleration = maxSpeed / accelerationTime;
+    }
+
     void Update()
     {
-        //transform.position = transform.position + velocity;
-        //if (Input.GetKeyDown(KeyCode.LeftArrow))
-        //{
-        //    transform.position = new Vector3(transform.position.x - 1f, transform.position.y);
-        //}
-        //if (Input.GetKeyDown(KeyCode.RightArrow))
-        //{
-        //    transform.position = new Vector3(transform.position.x + 1f, transform.position.y);
-        //}
-        //if (Input.GetKeyDown(KeyCode.UpArrow))
-        //{
-        //    transform.position = new Vector3(transform.position.x + transform.position.z , +1f);
-        //}
-        //if (Input.GetKeyDown(KeyCode.DownArrow))
-        //{
-        //    transform.position = new Vector3(transform.position.x + transform.position.z, -1f);
-        //}
+
 
         Vector3 offset = Vector3.zero;
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        
-            offset += Vector3.left * 0.01f;
-        
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        
-            offset += Vector3.right * 0.01f;
-        
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        
-            offset += Vector3.up * 0.01f;
-        
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        
-            offset += Vector3.down * 0.01f;
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+
+            offset += Vector3.left;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+
+            offset += Vector3.right;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+
+            offset += Vector3.up;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+
+            offset += Vector3.down;
+
+
+       //PlayerMovement(offset);
+        offset = offset.normalized; 
+
+       // inputDirection = inputDirection.normalized;
+
+        if (offset != Vector3.zero)
+
+        {
+            currentVelocity = Vector3.MoveTowards(currentVelocity, offset * maxSpeed, acceleration * Time.deltaTime);
+        }
+
+        else
+        {
+     
+            currentVelocity = Vector3.MoveTowards(currentVelocity, Vector3.zero, acceleration * Time.deltaTime);
+        }
+
+
+        transform.position += currentVelocity * Time.deltaTime;
 
     }
+
     private void PlayerMovement(Vector3 offset)
     {
         transform.position += offset;
-
     }
 }
+
+
+
+// Task 1
+
+//transform.position = transform.position + velocity;
+//if (Input.GetKeyDown(KeyCode.LeftArrow))
+//{
+//    transform.position = new Vector3(transform.position.x - 1f, transform.position.y);
+//}
+//if (Input.GetKeyDown(KeyCode.RightArrow))
+//{
+//    transform.position = new Vector3(transform.position.x + 1f, transform.position.y);
+//}
+//if (Input.GetKeyDown(KeyCode.UpArrow))
+//{
+//    transform.position = new Vector3(transform.position.x + transform.position.z , +1f);
+//}
+//if (Input.GetKeyDown(KeyCode.DownArrow))
+//{
+//    transform.position = new Vector3(transform.position.x + transform.position.z, -1f);
+//}
+
+//Vector3 offset = Vector3.zero;
+
+//if (Input.GetKey(KeyCode.LeftArrow))
+
+//    offset += Vector3.left * 0.01f;
+
+//if (Input.GetKey(KeyCode.RightArrow))
+
+//    offset += Vector3.right * 0.01f;
+
+//if (Input.GetKey(KeyCode.UpArrow))
+
+//    offset += Vector3.up * 0.01f;
+
+//if (Input.GetKey(KeyCode.DownArrow))
+
+//    offset += Vector3.down * 0.01f;
+
+
+//PlayerMovement(offset);
+
+
+//Task 2
