50e90e3ccd65e367d721101f6cac5ebbacd4f6cf
DESKTOP-KBV0CC1\Eng <sathorn@sheridancollege.ca>
2024-09-24 22:11:20 -0400

    Redid Journal 3

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -4,23 +4,46 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    public float moveSpeed;
-    public float arrivalDistance;
-    public float maxFloatDistance;
+    public float moveSpeed = 1f;
+    public float arrivalDistance = 0.5f;
+    public float maxFloatDistance = 5f;
 
-    private Vector3 targetpoint;
+    private Vector3 targetPoint;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 random direction= 
-        currentVelocity = Vector3.MoveTowards(currentVelocity, offset * maxSpeed, acceleration * Time.deltaTime);
+        ChooseRandomPoint();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ( Player )
+        MoveTowardsTarget();
+
+        
+        if (Vector3.Distance(transform.position, targetPoint) <= arrivalDistance)
+        {
+          
+            ChooseRandomPoint();
+        }
     }
+
+    void ChooseRandomPoint()
+    {
+       
+        Vector3 randomDirection = Random.insideUnitSphere; 
+        randomDirection.z = 0;  
+        targetPoint = transform.position + randomDirection * Random.Range(0f, maxFloatDistance);
+    }
+
+    
+    void MoveTowardsTarget()
+    {
+       
+        Vector3 direction = (targetPoint - transform.position).normalized;
+        transform.position += direction * moveSpeed * Time.deltaTime;
+    }
+
 }
--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -7,8 +7,57 @@ public class Stars : MonoBehaviour
     public List<Transform> starTransforms;
     public float drawingTime;
 
+    private LineRenderer lineRenderer;
+    private int currentStarIndex = 0;
+
     // Update is called once per frame
     void Update()
     {
+    }
+    void Start()
+    {
+        
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.positionCount = 2;
+
+       
+
+    StartCoroutine(DrawLinesBetweenStars());
+    }
+
+    
+    IEnumerator DrawLinesBetweenStars()
+    {
+        while (true)
+        {
+            
+            int nextStarIndex = (currentStarIndex + 1) % starTransforms.Count;
+
+            
+            Vector3 startPoint = starTransforms[currentStarIndex].position;
+            Vector3 endPoint = starTransforms[nextStarIndex].position;
+
+            for (float t = 0; t < drawingTime; t += Time.deltaTime)
+            {
+                float progress = t / drawingTime;
+                Vector3 currentPoint = Vector3.Lerp(startPoint, endPoint, progress);
+
+                
+                lineRenderer.SetPosition(0, startPoint);
+                lineRenderer.SetPosition(1, currentPoint);
+
+                yield return null; 
+            }
+
+            
+            lineRenderer.SetPosition(0, startPoint);
+            lineRenderer.SetPosition(1, endPoint);
+
+           
+            currentStarIndex = nextStarIndex;
+
+           
+            yield return null;
+        }
     }
 }
